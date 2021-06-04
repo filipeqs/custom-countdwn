c28156dc120786a64354b98c698f06abda70a328
Filipe Silva <filipeqs@outlook.com>
2021-06-04 10:54:56 -0400

    reset countdown

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const inputContainer = document.getElementById('input-container');
-const countdownForm = document.getElementById('countdown-form');
-const dateEl = document.getElementById('date-picker');
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdown-form");
+const dateEl = document.getElementById("date-picker");
 
-const countdownEl = document.getElementById('countdown');
-const countdownElTitle = document.getElementById('countdown-title');
-const countDownBtn = document.getElementById('countdown-button');
-const timeElements = document.querySelectorAll('span');
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countDownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll("span");
 
-let countdownTitle = '';
-let countdownDate = '';
+let countdownTitle = "";
+let countdownDate = "";
 let countdownValue = Date;
+let countdownActive;
 
 const second = 1000;
 const minute = second * 60;
@@ -17,11 +18,12 @@ const hour = minute * 60;
 const day = hour * 24;
 
 // Set Date Input Minimum with Today's Date
-const today = new Date().toISOString().split('T')[0];
-dateEl.setAttribute('min', today);
+const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute("min", today);
 
 // Populate Countdown / Complete UI
 const updateDOM = () => {
+  countdownActive = setInterval(() => {
     const now = new Date().getTime();
     const distance = countdownValue - now;
 
@@ -41,19 +43,40 @@ const updateDOM = () => {
     inputContainer.hidden = true;
     // Show Countdown
     countdownEl.hidden = false;
+  }, second);
 };
 
 // Take Values from Form Input
 const updateCountDown = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    countdownTitle = e.srcElement[0].value;
-    countdownDate = e.srcElement[1].value;
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
 
+  // Check for valid date
+  if (countdownDate === "") {
+    alert("Please select a date for the countdown.");
+  } else {
     // Get number version of current Date, updateDOM
     countdownValue = new Date(countdownDate).getTime();
     updateDOM();
+  }
+};
+
+// Reset All Values
+const reset = () => {
+  // Hide Countdowns, Show Input
+  countdownEl.hidden = true;
+  inputContainer.hidden = false;
+
+  // Stop the countdown
+  clearInterval(countdownActive);
+
+  // Reset Values
+  countdownTitle = "";
+  countdownDate = "";
 };
 
 // Event Listeners
-countdownForm.addEventListener('submit', updateCountDown);
+countdownForm.addEventListener("submit", updateCountDown);
+countDownBtn.addEventListener("click", reset);
